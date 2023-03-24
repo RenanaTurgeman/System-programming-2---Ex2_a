2a8bcce45876ece9c1814799a0409a5d26627cc8
RenanaTurgeman <renanaturgeman058@gmail.com>
2023-03-24 14:35:08 +0300

    solve mestake

--- sources/game.hpp
+++ sources/game.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include "player.hpp"
 
 namespace ariel{
     class Game
--- sources/player.cpp
+++ sources/player.cpp
@@ -6,12 +6,12 @@ Player::Player(std::string name){
     this->name = name;
 }
 int Player::stacksize(){ //prints the amount of cards left.
+return 1;
 
 }
 
 int Player::cardesTaken(){ //prints the amount of cards this player has won.
-
-
+return 0;
 }
 
 }//namescap
